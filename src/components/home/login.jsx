870d15c0bf7ba9308fffac2b723774a69a49973b
OEncouracado <tryironcl4d@gmail.com>
2023-09-27 17:26:58 -0300

    tentando fazer login 1

--- src/components/home/login.jsx
+++ src/components/home/login.jsx
@@ -3,12 +3,33 @@ import { Button, Container, Form, Image } from 'react-bootstrap'
 import './index.css'
 import logo from "../../images/logo.png"
 import axios from 'axios';
+import CryptoJS from 'crypto-js';
 
 function Login() {
   const [nome , setNome] = useState('');
   const [senha , setSenha] = useState(''); // eslint-disable-next-line
   const [mensagem, setMensagem] = useState('');// eslint-disable-next-line
   const [tipoMensagem, setTipoMensagem] = useState('');// eslint-disable-next-line
+
+  
+  const decryptAES = (encryptedData, password) => {
+    try {
+      const iv = CryptoJS.enc.Hex.parse(encryptedData.iv);
+      const encryptedBytes = CryptoJS.enc.Base64.parse(encryptedData.data); // Modificação aqui
+
+      const decryptedBytes = CryptoJS.AES.decrypt(
+          { ciphertext: encryptedBytes },
+          CryptoJS.enc.Utf8.parse(password),
+          { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 }
+      );
+
+      const decryptedText = decryptedBytes.toString(CryptoJS.enc.Utf8);
+      return JSON.parse(decryptedText); // Modificação aqui
+    } catch (error) {
+      console.error('Erro ao descriptografar:', error);
+      return null;
+    }
+  };
 
   useEffect(() => {
     if (mensagem) {
@@ -23,10 +44,26 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("https://hospitalemcor.com.br/claviscord/api/index.php?table=usuarios" , {nome , senha })
-    .then ((response)=>{
-      console.log(response)
+    axios.post("https://hospitalemcor.com.br/claviscord/api/index.php?table=usuarios" , {nome , senha }, {timeout: 5000})
+    .then(response => {
+      try {
+        console.log(response.data);
+        // Descriptografa os dados
+        const decryptedData = decryptAES(response.data, '0123456789ABCDEF0123456789ABCDEF');
+        console.log('Login bem-sucedido', decryptedData);
+        setMensagem(decryptedData.data);
+        setTipoMensagem('success');
+        const token = decryptedData.data.token;
+        // localStorage.setItem('token', token);
+        console.log(token);
+
+      } catch (error) {
+        console.error('Erro ao processar os dados:', error);
+      }
     })
+    .catch(error => {
+      console.error('Erro ao obter as chaves:', error);
+    });
   }
 
 
